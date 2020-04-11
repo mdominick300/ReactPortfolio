04b195e3601f181b28161cd7b79d268b976b135a
Matt Dominick <mdominick300@gmail.com>
2020-04-11 12:43:33 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
-import Resume from "./pages/Resume";
+// import Resume from "./pages/Resume";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
